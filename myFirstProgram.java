b1d5ee7c81f132c3530ff0d86309d44f6829469b
Ibraheem Noubani <ibraheem1031@gmail.com>
2020-09-23 09:35:57 -0400

    Ibraheem Noubani My name is

---
+++ myFirstProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class myFirstProgram {
+
+	public static void main(String[] args) { 
+		 
+		Scanner scanner = new Scanner(System.in); 
+		String input = scanner.next(); 
+		scanner.close();
+		
+		System.out.println(" " + input); 
+	} 
+}
